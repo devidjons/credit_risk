28f5ad8603ec4784ac96e6b2a24d50e5b67aec75
vlad <devidjonsik@gmail.com>
2018-11-20 09:52:39 +0100

    still not working

--- cw7.R
+++ cw7.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(animation)
 library(rootSolve)#multiroot function
 dane = read.csv(
     "http://www2.im.uj.edu.pl/DariuszZawisza/enron.csv",
@@ -6,8 +7,8 @@ dane = read.csv(
     header = T,
     dec = ","
 )
-
-d1 = function(A, L, r, sigma)
+colnames(dane) = c("E", "L", "r", "sp")
+d1 = function(, L, r, sigma)
 {
     return((log(A / L) + (r + (sigma ^ 2) / 2)) / sigma)
 }
@@ -18,20 +19,16 @@ d2 = function(A, L, r, sigma)
 }
 
 sigma_E=sd(diff(log(dane$equities)))*sqrt(262)
-f=function(a)
+f1=function(x, data=dane)
 {
-    x=a[,1]
-    y=a[,2]
-    sum(x)+sum(y^2)
+    E = data$E
+    L = data$L
+    r = data$r
+    return(c((E - x[1:262] * pnorm(d1(x[1:262], L, r, x[263:524])) + L * exp(-r) * pnorm(d2(x[1:262], L, r, x[263:524]))) ^ 2,
+              (x[1:262] * pnorm(d1(x[1:262], L, r, x[263:524])) * x[263:524] - sigma_E * E) ^ 2))
+    
+    
 }
-g=function(a)
-{
-    x=a[,1]
-    y=a[,2]
-    sum(x-y)
-}
-model=function(a)
-{
-    return(c(f(a), g(a)))
-}
-multiroot(model, start=matrix(rnorm(4),2,2))
+
+multiroot(f1, start=c(rep(1, 262), rep(0.1,262)), data=dane)
+newton.method(f1, init=c(rep(1, 262), rep(0.1,262)))
