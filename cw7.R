5747b63cec40e4d34160a265ef3dedad3d9e5f09
vlad <devidjonsik@gmail.com>
2018-11-20 16:13:13 +0100

    cw7

--- cw7.R
+++ cw7.R
@@ -8,7 +8,7 @@ dane = read.csv(
     dec = ","
 )
 colnames(dane) = c("E", "L", "r", "sp")
-d1 = function(, L, r, sigma)
+d1 = function(A, L, r, sigma)
 {
     return((log(A / L) + (r + (sigma ^ 2) / 2)) / sigma)
 }
@@ -19,16 +19,16 @@ d2 = function(A, L, r, sigma)
 }
 
 sigma_E=sd(diff(log(dane$equities)))*sqrt(262)
-f1=function(x, data=dane)
+f1=function(x,n, data=dane)
 {
-    E = data$E
-    L = data$L
-    r = data$r
-    return(c((E - x[1:262] * pnorm(d1(x[1:262], L, r, x[263:524])) + L * exp(-r) * pnorm(d2(x[1:262], L, r, x[263:524]))) ^ 2,
-              (x[1:262] * pnorm(d1(x[1:262], L, r, x[263:524])) * x[263:524] - sigma_E * E) ^ 2))
+    E = data$E[n]
+    L = data$L[n]
+    r = data$r[n]
+    return(c(E - x[1] * pnorm(d1(x[1], L, r, x[2])) + L * exp(-r) * pnorm(d2(x[1], L, r, x[2])),
+              x[1] * pnorm(d1(x[1], L, r, x[2])) * x[2] - sigma_E * E))
     
     
 }
 
-multiroot(f1, start=c(rep(1, 262), rep(0.1,262)), data=dane)
-newton.method(f1, init=c(rep(1, 262), rep(0.1,262)))
+multiroot(function(x) f1(x,1,dane), start=c(1, 0.1))
+ newton.method(f1, init=c(rep(1, 262), rep(0.1,262)))
