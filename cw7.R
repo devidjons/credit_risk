e19154ee3afe658ef97f6961a1d3013d25b55d04
vlad <devidjonsik@gmail.com>
2018-11-26 18:13:57 +0100

    final

--- cw7.R
+++ cw7.R
@@ -20,9 +20,8 @@ d2 = function(A, L, r, sigma)
 }
 
 sigma_E=sd(diff(log(dane$E)))*sqrt(262)
-f1=function(a1,a2,n, data=dane)
+f1=function(x,n, data=dane)
 {
-    x=c(a1,a2)
     E = data$E[n]
     L = data$L[n]
     r = data$r[n]
@@ -30,25 +29,7 @@ f1=function(a1,a2,n, data=dane)
               x[1] * pnorm(d1(x[1], L, r, x[2])) * x[2] - sigma_E * E))
 }
 
+solution = sapply(1:dim(dane)[1], function(x)
+    multiroot(function(y)
+        f1(y, n = x), start = c(60000, 0.1))$root) %>% t
 
-
-
-ga_fun = function(n)
-{
-    r1 = ga(
-        type = "real-valued",
-        fitness = function(x)
-            - sum(f1(x[1], x[2], n) ^ 2),
-        lower = c(dane$E[n], 0.1),
-        upper = c(dane$E[n] + dane$L[n], 0.6),
-        popSize = 100,
-        maxiter = 500,
-        maxFitness = -2e-2,
-        suggestions = matrix(rep(c((dane$E[n] + dane$L[n]) * 0.95, 0.3), 4), ncol = 2, byrow = T),
-        keepBest = F
-    )
-    sol=r1@solution
-    return(c(sol, sum(f1(sol[1], sol[2], n) ^ 2)))
-}
-r2=ga_fun(5)
-solution=sapply(1:262, function(x) ga_fun(x))%>%t
