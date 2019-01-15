b5465b5e7602159ae5e6ad162ccef7ed86d60aa2
vlad <devidjonsik@gmail.com>
2019-01-15 10:23:54 +0100

    zrobione cw12

---
+++ cw12.R
@@ -0,0 +1,6 @@
+library(dplyr)
+r = data.frame(
+    t = c(0.0833, 0.25, 1, 2, 3, 5, 10),
+    R = 0.01 * c(5.56, 5.64, 5.68, 5.64, 5.62, 5.66, 5.76)
+)
+r %>% mutate(B_0_T = 1 * exp(-R * t)) %>% select(t, B_0_T) -> data_for_regression
